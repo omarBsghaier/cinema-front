2067ee0c1fe43bec9f6d4c11e60a0809a04c8eed
omarBsghaier <omar.bensghaier@etudiant-fst.utm.tn>
2023-05-28 12:49:28 +0100

    "front cinema"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CinemaComponent } from './cinema/cinema.component';
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CinemaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    RouterLink,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
